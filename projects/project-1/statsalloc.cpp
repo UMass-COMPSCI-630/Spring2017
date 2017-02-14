bce36a50b8b5d3204ee38ee251aaf38e8b7da708
emeryberger <emery.berger@gmail.com>
2017-02-14 15:15:27 -0500

    Removed const.

--- projects/project-1/statsalloc.cpp
+++ projects/project-1/statsalloc.cpp
@@ -9,36 +9,36 @@ void StatsAlloc<SourceHeap>::free(void * ptr) {
 }
 
 template <class SourceHeap>
-size_t StatsAlloc<SourceHeap>::getSize(void * p) const {
+size_t StatsAlloc<SourceHeap>::getSize(void * p) {
   return 0; // FIX ME
 }
 
 // number of bytes currently allocated  
 template <class SourceHeap>
-size_t StatsAlloc<SourceHeap>::bytesAllocated() const {
+size_t StatsAlloc<SourceHeap>::bytesAllocated() {
   return 0; // FIX ME
 }
 
 // max number of bytes allocated  
 template <class SourceHeap>
-size_t StatsAlloc<SourceHeap>::maxBytesAllocated() const {
+size_t StatsAlloc<SourceHeap>::maxBytesAllocated() {
   return 0; // FIX ME
 }
 
 // number of bytes *requested* (e.g., malloc(4) might result in an allocation of 8; 4 = bytes requested, 8 = bytes allocated)
 template <class SourceHeap>
-size_t StatsAlloc<SourceHeap>::bytesRequested() const {
+size_t StatsAlloc<SourceHeap>::bytesRequested() {
   return 0; // FIX ME
 }
   
 // max number of bytes *requested*
 template <class SourceHeap>
-size_t StatsAlloc<SourceHeap>::maxBytesRequested() const {
+size_t StatsAlloc<SourceHeap>::maxBytesRequested() {
   return 0; // FIX ME
 }
 
 template <class SourceHeap>
-void StatsAlloc<SourceHeap>::walk(const std::function< void(Header *) >& f) const {
+void StatsAlloc<SourceHeap>::walk(const std::function< void(Header *) >& f) {
   // FIX ME
 }
 
