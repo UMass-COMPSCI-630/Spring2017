4d243e24efcf3dfc542a1287c5aea4cb2fd84d2e
emeryberger <emery.berger@gmail.com>
2017-03-11 13:21:36 -0500

    Added.

---
+++ projects/project-2/mmapheap.h
@@ -0,0 +1,51 @@
+#ifndef MMAPHEAP_H
+#define MMAPHEAP_H
+
+#include <sys/mman.h>
+
+template <size_t Size>
+class MmapHeap {
+public:
+  MmapHeap()
+    : heapRemaining (Size)
+  {
+    origHeapStart = (char *) mmap((void *) 0, Size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (origHeapStart == (char *) -1) {
+      perror("Map failed");
+      origHeapStart = nullptr;
+      heapRemaining = 0;
+    }
+    heapStart = origHeapStart;
+  }
+  ~MmapHeap() {
+    if (origHeapStart != nullptr) {
+      munmap(origHeapStart, Size);
+    }
+  }
+  size_t getSize() { return Size; }
+  void * getStart() {
+    return origHeapStart;
+  }
+  void * malloc(size_t sz) {
+    if (heapRemaining < sz) {
+      return nullptr;
+    }
+    char * p = heapStart;
+    heapStart += sz;
+    heapRemaining -= sz;
+    return (void *) p;
+  }
+  size_t getRemaining() {
+    return heapRemaining;
+  }
+  
+private:
+  void free(void *);
+  size_t getSize(void *);
+  char * origHeapStart;
+  char * heapStart;
+  size_t heapRemaining;
+};
+
+
+#endif
