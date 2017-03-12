c76f2132df2a4866a4acea1a4d698dc1a92532fd
emeryberger <emery.berger@gmail.com>
2017-03-11 21:27:46 -0500

    More tests.

--- projects/project-2/testme.cpp
+++ projects/project-2/testme.cpp
@@ -8,14 +8,21 @@ extern "C"
   //  void xxfree(void *);
 }
 
+int testme;
+
 int main()
 {
+  cout << (size_t) &testme << endl;
   cout << "-----------" << endl;
   int ** p1 = (int **) malloc(8);
   int * p2 = (int *) malloc(8);
+  char * p = nullptr;
+  cout << "p1 address = " << (size_t) &p1 << ", p2 address = " << (size_t) &p2 << endl;
+  cout << "p address = " << (size_t) &p << endl;
+  cout << "p1 should be reachable: " << (size_t) p1 << endl;
+  cout << "p2 should be reachable: " << (size_t) p2 << endl;
   *p1 = p2;
   *p2 = 12;
-  char * p = nullptr;
   for (int i = 0; i < 200000; i++) {
     char * ptr = (char *) malloc(256);
     ptr[0] = 'X';
